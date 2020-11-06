7ea39d33655afaaf3e13da637a587570fe48ec73
eyaadh <eyaadh@gmail.com>
2020-11-06 11:58:35 +0500

    code style improvements

--- main.py
+++ main.py
@@ -61,9 +61,9 @@ async def crop_to_circle(im):
 @some_sticker_bot.on_message(filters.command("start"))
 async def start_handler(c: Client, m: Message):
     await m.reply_text(
-        f"Hi, I just create telegram sticker from the text messages you send me. \nMy creator @eyaadh did a YouTube "
-        f"[video](https://youtu.be/dVrA9hit4ks) on how he created me. The link for my source is on the video "
-        f"description, you can fork the project and make a better version of me.",
+        "Hi, I just create telegram sticker from the text messages you send me. \nMy creator @eyaadh did a YouTube "
+        "[video](https://youtu.be/dVrA9hit4ks) on how he created me. The link for my source is on the video "
+        "description, you can fork the project and make a better version of me.",
         disable_web_page_preview=True
     )
 
@@ -71,10 +71,10 @@ async def start_handler(c: Client, m: Message):
 @some_sticker_bot.on_message(filters.command("help"))
 async def help_handler(c: Client, m: Message):
     await m.reply_text(
-        f"Hi, I do not have much to say on help - I just create telegram stickers from the text messages you send me. "
-        f"\nMy creator @eyaadh did a YouTube "
-        f"[video](https://youtu.be/dVrA9hit4ks) on how he created me. The link for my source is on the video "
-        f"description, you can fork the project and make a better version of me.",
+        "Hi, I do not have much to say on help - I just create telegram stickers from the text messages you send me. "
+        "\nMy creator @eyaadh did a YouTube "
+        "[video](https://youtu.be/dVrA9hit4ks) on how he created me. The link for my source is on the video "
+        "description, you can fork the project and make a better version of me.",
         disable_web_page_preview=True
     )
 
